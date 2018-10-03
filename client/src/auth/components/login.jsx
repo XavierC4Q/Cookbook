1a5a467d016499bbf761d91da1a1feaec9c3f2f5
Xavier <xaviermunroe@ac.c4q.nyc>
2018-10-03 15:31:32 -0400

    added forms and redirects

--- client/src/auth/components/login.jsx
+++ client/src/auth/components/login.jsx
@@ -1,14 +1,51 @@
 import React from 'react'
+import { withRouter, Redirect } from 'react-router-dom'
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { default as dispatches } from '../redux/dispatches'
 
-const Login = () => {
-    return (
-        <div></div>
-    )
+class Login extends React.Component {
+    render() {
+        const { handleSubmit, loginUser, currentUser } = this.props
+        if(currentUser){
+            return (<Redirect to='/cookbook'/>)
+        }
+        return (
+            <div>
+                <h1>WELCOME BACK, LOGIN BELOW</h1>
+                <form onSubmit={handleSubmit(loginUser)}>
+                    <div>
+                        <label>USERNAME</label>
+                        <Field name='username' component='input' />
+                    </div>
+                    <div>
+                        <label>PASSWORD</label>
+                        <Field name='password' component='input' />
+                    </div>
+                    <button type='submit'>SUBMIT</button>
+                </form>
+            </div>
+        )
+    }
 }
 
-Login = reduxForm({
-    form: 'login'
-})
+const mapStateToProps = state => {
+    return {
+        currentUser: state.auth.currentUser
+    }
+}
 
-export default Login
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loginUser: (values) => {
+            dispatch(dispatches.loginUser(values))
+        }
+    }
+}
+
+Login = connect(mapStateToProps, mapDispatchToProps)(Login)
+
+export default withRouter(reduxForm({
+    form: 'Login'
+})(Login))
+
--- client/src/auth/components/register.jsx
+++ client/src/auth/components/register.jsx
@@ -1,14 +1,66 @@
 import React from 'react'
+import { withRouter, Redirect } from 'react-router-dom'
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { default as dispatches } from '../redux/dispatches'
 
-const Register = () => {
-    return (
-        <div></div>
-    )
+class Register extends React.Component {
+    render() {
+        const { handleSubmit, registerUser, currentUser } = this.props
+        if(currentUser){
+            return (<Redirect to='/cookbook'/>)
+        }
+        return (
+            <div>
+                <h1>REGISTER BELOW</h1>
+                <form onSubmit={handleSubmit(registerUser)}>
+                    <div>
+                        <label>USERNAME</label>
+                        <Field name='username' component='input' />
+                    </div>
+                    <div>
+                        <label>PASSWORD</label>
+                        <Field name='password' component='input' />
+                    </div>
+                    <div>
+                        <label>EMAIL</label>
+                        <Field name='email' component='input' />
+                    </div>
+                    <div>
+                        <label>FIRST NAME</label>
+                        <Field name='firstName' component='input' />
+                    </div>
+                    <div>
+                        <label>LAST NAME</label>
+                        <Field name='lastName' component='input' />
+                    </div>
+                    <div>
+                        <label>AGE</label>
+                        <Field name='age' component='input' />
+                    </div>
+                    <button type='submit'>SUBMIT</button>
+                </form>
+            </div>
+        )
+    }
 }
 
-Register = reduxForm({
-    form: 'register'
-})
+const mapStateToProps = state => {
+    return {
+        currentUser: state.auth.currentUser
+    }
+}
 
-export default Register
+const mapDispatchToProps = (dispatch) => {
+    return {
+        registerUser: (values) => {
+            dispatch(dispatches.registerUser(values))
+        }
+    }
+}
+
+Register = connect(mapStateToProps, mapDispatchToProps)(Register)
+
+export default withRouter(reduxForm({
+    form: 'Register'
+})(Register))
