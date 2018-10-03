afc04bf5aeed7475df81f38e94e0bb5a855dcb88
Xavier <xaviermunroe@ac.c4q.nyc>
2018-10-03 15:30:49 -0400

    added base routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import Login from './auth/components/login'
+import Register from './auth/components/register'
+import Landing from './landing/components/landing'
+import Home from './home/home'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>STARTING PAGE HERE</h1>
+        <Route exact path='/' component={Landing}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/register' component={Register}/>
+        <Route path='/cookbook' component={Home}/>
       </div>
     );
   }
