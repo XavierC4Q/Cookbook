d8f99f03ce2b8c10a952dd32854aa09c0392a508
Xavier <xaviermunroe@ac.c4q.nyc>
2018-10-03 15:29:34 -0400

    fixed silly error with dispatch missing from function call

--- client/src/auth/redux/dispatches.js
+++ client/src/auth/redux/dispatches.js
@@ -1,6 +1,55 @@
-import actions from './actions'
+import { default as actions } from './actions'
 import axios from 'axios'
 
 const loginUser = (values) => {
+    return (dispatch) => {
+        const {
+            username,
+            password
+        } = values
 
+        axios.post('/login', {
+                username: username,
+                password: password
+            })
+            .then(res => {
+                dispatch(actions.login(res.data))
+            })
+            .catch(error => {
+                dispatch(actions.userAuthError(error))
+            })
+    }
+}
+
+const registerUser = (values) => {
+    return (dispatch) => {
+        const {
+            username,
+            password,
+            email,
+            firstName,
+            lastName,
+            age
+        } = values
+
+        axios.post('/register', {
+            username: username,
+            password: password,
+            age: age,
+            email: email,
+            firstName: firstName,
+            lastName: lastName
+        })
+        .then(res => {
+            dispatch(actions.register(res.data))
+        })
+        .catch(error => {
+            dispatch(actions.userAuthError(error))
+        })
+    }
+}
+
+export default {
+    loginUser,
+    registerUser
 }
