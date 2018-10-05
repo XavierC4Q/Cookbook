e9f569bb1969185333809874ff94f073aaa214b1
Xavier <xaviermunroe@ac.c4q.nyc>
2018-10-05 17:26:59 -0400

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import { Switch, Route } from 'react-router-dom'
 import Login from './auth/components/login'
 import Register from './auth/components/register'
 import Landing from './landing/components/landing'
-import Home from './home/home'
+import Feed from './feed/components/feed'
+import Profile from './profile/components/profile'
+import AddRecipe from './profile/components/addRecipe'
 
 class App extends Component {
   render() {
@@ -13,7 +15,12 @@ class App extends Component {
         <Route exact path='/' component={Landing}/>
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
-        <Route path='/cookbook' component={Home}/>
+        <Route path='/cookbook' component={Feed}/>
+        <Route path='/profile/:username' render={props => {
+          const { username } = props.match.params
+          return (<Profile username={username}/>)
+        }}/>
+        <Route path='/addRecipe' component={AddRecipe}/>
       </div>
     );
   }
